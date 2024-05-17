65c7a74c0f11eebbbf44827f587c8eb3aeaaa35b
amasirat <amir.basirat.2004@gmail.com>
2024-05-17 18:46:16 +0330

    added README

--- .godot/mono/temp/obj/Debug/DodgeTheCreeps.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/DodgeTheCreeps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DodgeTheCreeps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4759f2d2d0eca2148f73d2272462d7a3616be12e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DodgeTheCreeps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DodgeTheCreeps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,3 @@
+# Dodge The Creeps
+
+The final project created by following along the Dodge The Creeps 2D Step by Step guide in the Godot docs created using the C# language. Located [here](https://docs.godotengine.org/en/stable/getting_started/first_2d_game/index.html).
